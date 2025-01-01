bdbe811dbc938b42338737b0c3a5dc7055e718cd
German Lugo <g.lugo.dev@gmail.com>
2025-01-01 21:47:06 +0100

    Add x and X cases.

--- ft_printf.c
+++ ft_printf.c
@@ -2,9 +2,8 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdlib.h> // malloc, free
 
-#define F_SPEC "csdip" //uxX"
+#define F_SPEC "csdixX" //pu
 
 static int	ft_get_num_length(long n)
 {
@@ -73,7 +72,6 @@ char	*ft_itoa(int n)
 	return (character);
 }
 
-
 int	ft_putchar(char *s)
 {
 	if (s && *s != '\0')
@@ -131,6 +129,66 @@ int	ft_putnum(int num)
 	return (len);
 }
 
+int	hex_length(int decimal)
+{
+	int	length;
+
+	length = 0;
+	while (decimal > 0)
+	{
+		decimal /= 16;
+		length++;
+	}
+	return (length);
+}
+
+int	cal_current_power(int i)
+{
+	int current_power = 1;
+	int j = 0;
+	while (j < i)
+	{
+		current_power *= 16;
+		j++;
+	}
+	return current_power;
+}
+
+char	get_hex_digit(int digit, char f)
+{
+	if (digit < 10)
+		return '0' + digit;
+	else if (f == 'X')
+		return 'A' + (digit - 10);
+	else
+		return 'a' + (digit - 10);
+}
+
+int	dec_to_hex(int decimal, char f)
+{
+	int		len;
+	char	 *hexadecimal;
+	int		i;
+	int		current_power;
+	int		digit;
+
+	len = hex_length(decimal);
+	hexadecimal = malloc(sizeof(char) * (len + 1));
+	hexadecimal[len] = '\0';
+	i = len - 1;
+	while (i >= 0)
+	{
+		current_power = cal_current_power(i);
+		digit = decimal / current_power;
+		decimal -= digit * current_power;
+		hexadecimal[len - 1 - i] = get_hex_digit(digit, f);
+		i--;
+	}
+	ft_putstr(hexadecimal);
+	free(hexadecimal);
+	return (len);
+}
+
 int	f_selector(va_list *args, const char *format)
 {
 	char	*s;
@@ -142,14 +200,11 @@ int	f_selector(va_list *args, const char *format)
 		return (ft_putchar(&s[0]));
 	} 
 	else if (format[1] == 's')
-	{
-		s = va_arg(*args, char *);
-		return (ft_putstr(s));
-	}
+		return (ft_putstr(va_arg(*args, char *)));
 	else if (format[1] == 'd' || format[1] == 'i')
-	{
 		return (ft_putnum(va_arg(*args, int)));
-	}
+	else if (format[1] == 'x' || format[1] == 'X')
+		return (dec_to_hex(va_arg(*args, int), format[1]));
 	else
 		s = NULL;
 	free(s);
@@ -187,17 +242,49 @@ int	ft_printf(char *f, ...)
 
 int	main(void)
 {
-	// int	number_of_char;
-	// int	number_of_char_2;
-
-	// number_of_char = ft_printf("Hola%c %s %d %i", '!', "mundo", 42, 100000);
-	// printf("\n%i\n", number_of_char);
-	// number_of_char = ft_printf("El número máximo para un int es: %d", 2147483647);
-	// printf("\n%i\n", number_of_char);
-	// number_of_char = 
-	ft_printf("Número mínimo para un int es: %d", -2147483647);
-	// printf("El número mínimo para un int es: %d", -2147483647);
-	// printf("\n%i\n", number_of_char);
-	// number_of_char_2 = printf("Hola%c%", '!');
-	// printf("\n%i\n", number_of_char_2);
+	ft_printf("Hexadecimal edge cases:\n");
+	ft_printf("0x%x\n", 0);
+	printf("0x%x\n", 0);
+	ft_printf("0x%x\n", 1);
+	printf("0x%x\n", 1);
+	ft_printf("0x%x\n", 15);
+	printf("0x%x\n", 15);
+	ft_printf("0x%x\n", 16);
+	printf("0x%x\n", 16);
+	ft_printf("0x%x\n", 255);
+	printf("0x%x\n", 255);
+	ft_printf("0x%x\n", 256);
+	printf("0x%x\n", 256);
+	ft_printf("0x%x\n", 4095);
+	printf("0x%x\n", 4095);
+	ft_printf("0x%x\n", 4096);
+	printf("0x%x\n", 4096);
+	ft_printf("0x%x\n", 65535);
+	printf("0x%x\n", 65535);
+	ft_printf("0x%x\n", 65536);
+	printf("0x%x\n", 65536);
+	ft_printf("0x%x\n", -1);
+	printf("0x%x\n", -1);
+	ft_printf("0x%X\n", 0);
+	printf("0x%X\n", 0);
+	ft_printf("0x%X\n", 1);
+	printf("0x%X\n", 1);
+	ft_printf("0x%X\n", 15);
+	printf("0x%X\n", 15);
+	ft_printf("0x%X\n", 16);
+	printf("0x%X\n", 16);
+	ft_printf("0x%X\n", 255);
+	printf("0x%X\n", 255);
+	ft_printf("0x%X\n", 256);
+	printf("0x%X\n", 256);
+	ft_printf("0x%X\n", 4095);
+	printf("0x%X\n", 4095);
+	ft_printf("0x%X\n", 4096);
+	printf("0x%X\n", 4096);
+	ft_printf("0x%X\n", 65535);
+	printf("0x%X\n", 65535);
+	ft_printf("0x%X\n", 65536);
+	printf("0x%X\n", 65536);
+	ft_printf("0x%X\n", -1);
+	printf("0x%X\n", -1);
 }
