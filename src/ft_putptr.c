b27e1d444661f00dcc490f5e19953a77e196f537
German G. Lugo Munoz <glugo-mu@42firenze.it>
2025-01-13 19:02:34 +0100

    Fix major bugs.

--- src/ft_putptr.c
+++ src/ft_putptr.c
@@ -6,40 +6,75 @@
 /*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 21:47:40 by glugo-mu          #+#    #+#             */
-/*   Updated: 2025/01/13 14:20:47 by glugo-mu         ###   ########.fr       */
+/*   Updated: 2025/01/13 19:02:07 by glugo-mu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_putptr(void *ptr)
+static int	ptr_length(unsigned long d)
 {
-	unsigned long	addr;
-	char			*buffer;
-	int				len;
-	int				started;
-	int				i;
-	int				digit;
+	int	length;
 
-	len = 0;
-	buffer = malloc(sizeof(void *) * 21);
-	buffer[len++] = '0';
-	buffer[len++] = 'x';
-	addr = (unsigned long)ptr;
-	started = 0;
-	i = (sizeof(addr) * 2);
+	length = 0;
+	while (d > 0)
+	{
+		d /= 16;
+		length++;
+	}
+	return (length + 2);
+}
+
+static unsigned long	cal_current_power(int i)
+{
+	unsigned long	current_power;
+	int				j;
+
+	j = 0;
+	current_power = 1;
+	while (j < i)
+	{
+		current_power *= 16;
+		j++;
+	}
+	return (current_power);
+}
+
+static void	dec_to_hex_cal_ptr(int len, unsigned long dec, char *hex, char f_s)
+{
+	int				i;
+	unsigned long	current_power;
+	unsigned long	digit;
+
+	digit = dec;
+	i = len - 1;
 	while (i >= 0)
 	{
-		digit = (addr >> (i * 4)) & 0xF;
-		if (digit != 0 || started)
-		{
-			buffer[len++] = get_hex_digit(digit, 'x');
-			started = 1;
-		}
+		current_power = cal_current_power(i);
+		digit = dec / current_power;
+		dec -= digit * current_power;
+		hex[len - 1 - i] = get_hex_digit(digit, f_s);
 		i--;
 	}
-	if (!started)
-		buffer[len++] = '\0';
-	buffer[len] = '\0';
+}
+
+int	ft_putptr(void *ptr)
+{
+	int		len;
+	char	*hex;
+
+	if (ptr == NULL)
+	{
+		write(1, "(nil)", 5);
+		return (5);
+	}
+	len = ptr_length((unsigned long)ptr);
+	hex = malloc(sizeof(char) * (len + 1));
+	dec_to_hex_cal_ptr(len, (unsigned long)ptr, hex, 'x');
+	hex[0] = '0';
+	hex[1] = 'x';
+	hex[len + 1] = '\0';
+	ft_putstr(hex);
+	free(hex);
 	return (len);
 }
