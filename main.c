43fa88fac0ceecde7729dbdeb9b0b2896dd2cdcf
German G. Lugo Munoz <glugo-mu@42firenze.it>
2025-01-13 14:39:39 +0100

    Add test cases.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 22:34:53 by glugo-mu          #+#    #+#             */
-/*   Updated: 2025/01/09 21:01:29 by glugo-mu         ###   ########.fr       */
+/*   Updated: 2025/01/13 14:36:18 by glugo-mu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,28 +56,71 @@ int main(void)
 	num_char = printf("%i", INT_MIN);
 	printf("\nNumber of characters printed: %d\n", num_char);
 
+	num_char = ft_printf("%u", 255);
+	printf("\nNumber of characters printed: %d\n", num_char);
+	num_char = printf("%u", 255);
+	printf("\nNumber of characters printed: %d\n", num_char);
+
+	num_char = ft_printf("%u", -255);
+	printf("\nNumber of characters printed: %d\n", num_char);
+	num_char = printf("%u", -255);
+	printf("\nNumber of characters printed: %d\n", num_char);
+
 	num_char = ft_printf("%x", 255);
 	printf("\nNumber of characters printed: %d\n", num_char);
 	num_char = printf("%x", 255);
-	printf("\nNumber of characters printed: %d\n", num_char);
-
-	num_char = ft_printf("%X", 255);
-	printf("\nNumber of characters printed: %d\n", num_char);
-	num_char = printf("%X", 255);
-	printf("\nNumber of characters printed: %d\n", num_char);
+	printf("\nNumber of characters printed: %d\n", num_char); 
 
 	num_char = ft_printf("%x", -255);
 	printf("\nNumber of characters printed: %d\n", num_char);
 	num_char = printf("%x", -255);
-	printf("\nNumber of characters printed: %d\n", num_char); */
-
-	num_char = ft_printf("%x", -255);
 	printf("\nNumber of characters printed: %d\n", num_char);
-	num_char = printf("%x", -255);
+
+	num_char = ft_printf("%X", 43981);
+	printf("\nNumber of characters printed: %d\n", num_char);
+	num_char = printf("%X", 43981);
 	printf("\nNumber of characters printed: %d\n", num_char);
 
 	num_char = ft_printf("%X", -255);
 	printf("\nNumber of characters printed: %d\n", num_char);
 	num_char = printf("%X", -255);
 	printf("\nNumber of characters printed: %d\n", num_char);
+
+	num_char = ft_printf("%");
+	printf("\nNumber of characters printed: %d\n", num_char);
+	num_char = printf("%%");
+	printf("\nNumber of characters printed: %d\n", num_char); */
+
+    // Caso 1: Pasar un puntero nulo a %s
+    /*char *str1 = NULL;
+    printf("Caso 1: %s\n", str1); // Imprime "(null)"
+    ft_printf("Caso 1: %s\n", str1);
+
+    // Caso 2: Usar %p con un puntero nulo
+    void *ptr1 = NULL;
+    printf("Caso 2: %p\n", ptr1); // Imprime "(nil)" en algunas implementaciones
+    ft_printf("Caso 2: %p\n", ptr1);
+
+    // Caso 4: Array de cadenas mal inicializado
+    char *strings[3] = { "Hello", NULL, "World" };
+    printf("Caso 4: %s\n", strings[1]); // Imprime "(null)"
+    ft_printf("Caso 4: %s\n", strings[1]);
+
+    // Caso 5: Formato incorrecto con punteros genéricos
+    void *ptr2 = NULL;
+    printf("Caso 5: %s\n", (char *)ptr2); // Imprime "(null)"
+    ft_printf("Caso 5: %s\n", (char *)ptr2);
+
+    // Caso 6: String literal vacío combinado con puntero nulo
+    printf("Caso 6 (cadena vacía): %s\n", str1 ?: ""); // Imprime cadena vacía ""
+    ft_printf("Caso 6 (literal '(nil)'): %s\n", str1 ?: "(nil)");
+
+    // Caso 7: Cadenas nulas dentro de estructuras
+    struct example {
+        char *name;
+    };
+    struct example ex = { NULL };
+    printf("Caso 7: %s\n", ex.name); // Imprime "(null)"
+
+    return (0);*/
 }
